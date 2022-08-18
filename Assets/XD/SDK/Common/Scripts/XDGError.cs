9e55133ee34d9d909dd78926e14d6a52578f184a
guiming <suguiming@xd.com>
2022-08-18 17:32:03 +0800

    extra data to dic

--- Assets/XD/SDK/Common/Scripts/XDGError.cs
+++ Assets/XD/SDK/Common/Scripts/XDGError.cs
@@ -12,7 +12,7 @@ namespace XD.SDK.Common{
     public class XDGError{
         public int code; //40021: 邮箱未做验证, 40902: 邮箱冲突，第三方登录方式绑定邮箱关联账号已存在当前登录方式的另一个账号绑定。这两个code游戏需要弹框提示！！
         public string error_msg;
-        public string extra_data; //空 或者 是json。 参考： https://stg-xdsdk.ap-sg.tdsapps.com/docs/account/account-server#taptap-%E7%BB%91%E5%AE%9A%E9%82%AE%E7%AE%B1%E6%9C%AA%E9%AA%8C%E8%AF%81
+        public Dictionary<string, object> extra_data; //参考： https://stg-xdsdk.ap-sg.tdsapps.com/docs/account/account-server#taptap-%E7%BB%91%E5%AE%9A%E9%82%AE%E7%AE%B1%E6%9C%AA%E9%AA%8C%E8%AF%81
 
         public XDGError(Dictionary<string, object> dic){
             if (dic != null){
@@ -25,7 +25,7 @@ namespace XD.SDK.Common{
                 if (dic.ContainsKey(@"extra_data")){
                     var dataObj = dic["extra_data"]; //有可能空字符串
                     if (dataObj != null && dataObj.GetType() == typeof(Dictionary<string, object>)){
-                         extra_data = JsonConvert.SerializeObject(dataObj);
+                        extra_data = dataObj as Dictionary<string, object>;
                     }
                 }
             }
